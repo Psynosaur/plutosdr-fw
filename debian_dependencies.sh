9fa0a5eaf1265321dc4c8abc438730507f1dcc25
F5OEO <evaristec@gmail.com>
2024-01-03 22:47:14 +0100

    Add debian/unbuntu dependencies before make

---
+++ debian_dependencies.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y make gcc g++ python3 python3-dev python3-pip \
+           python3-pytest python3-numpy python3-scipy iverilog wget curl \
+           default-jre libxrender-dev libxtst-dev libtinfo5 \
+           libgtk2.0-0 git \
+           zip unzip dfu-util fakeroot u-boot-tools device-tree-compiler mtools \
+           bison flex libncurses5-dev libssl-dev bc cpio rsync cmake \
+           libgtk-3-0 xz-utils libgmp-dev libmpc-dev 
