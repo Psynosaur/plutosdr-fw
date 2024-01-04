134c2ed106309d72dbafe21413524bb5c39c1c9b
F5OEO <evaristec@gmail.com>
2024-01-04 14:28:18 +0100

    Add plutopluslinux

--- datv/applypatch.sh
+++ datv/applypatch.sh
@@ -2,3 +2,4 @@ cp patches/ad9361* ../linux/drivers/iio/adc/
 cp patches/zynq-pluto-sdr-maiasdr.dtsi ../linux/arch/arm/boot/dts/
 cp patches/oscimp/Makefile ../linux_driver/nco_counter_core/
 cp configs/zynq_pluto_linux_defconfig ../linux/arch/arm/configs/
+cp configs/zynq_plutoplus_linux_defconfig ../linux/arch/arm/configs/
