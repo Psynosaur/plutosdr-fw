5666218d11156e383fa122423d87879a259fe261
F5OEO <evaristec@gmail.com>
2024-01-05 15:54:49 +0100

    Remove TDD/PHAZER not in datv bistream

--- datv/applypatch.sh
+++ datv/applypatch.sh
@@ -1,6 +1,9 @@
 cp patches/ad9361* ../linux/drivers/iio/adc/
 cp patches/zynq-pluto-sdr-maiasdr.dtsi ../linux/arch/arm/boot/dts/
+#remove adm7111
 cp patches/zynq-pluto-sdr-revb.dts ../linux/arch/arm/boot/dts/
+#remove TDD/PHASER
+cp patches/zynq-pluto-sdr-revc.dts ../linux/arch/arm/boot/dts/
 cp patches/oscimp/Makefile ../linux_driver/nco_counter_core/
 cp configs/zynq_pluto_linux_defconfig ../linux/arch/arm/configs/
 cp configs/zynq_plutoplus_linux_defconfig ../linux/arch/arm/configs/
