787fa1a5c2f38042e50656c00faf06911155001a
F5OEO <evaristec@gmail.com>
2024-01-10 15:33:25 +0100

    Update to mosquitto 2.0.18 to try fixing segfault

--- datv/applypatch.sh
+++ datv/applypatch.sh
@@ -7,3 +7,5 @@ cp patches/zynq-pluto-sdr-revc.dts ../linux/arch/arm/boot/dts/
 cp patches/oscimp/Makefile ../linux_driver/nco_counter_core/
 cp configs/zynq_pluto_linux_defconfig ../linux/arch/arm/configs/
 cp configs/zynq_plutoplus_linux_defconfig ../linux/arch/arm/configs/
+## Replace mosquitoo 2.0.17 (segfault) with 20.0.18
+cp patches/mosquitto/* ../buildroot/package/mosquitto/
