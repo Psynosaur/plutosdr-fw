9a1ec92b48619d13239ff12d15cbb0dbef018f61
F5OEO <evaristec@gmail.com>
2024-01-16 19:01:48 +0100

    Fix NCO, and ptt for sdrconsole

--- datv/board/pluto/overlay/root/watchconsoletx.sh
+++ datv/board/pluto/overlay/root/watchconsoletx.sh
@@ -4,20 +4,20 @@
 ptton()
 {
     #PTT on GPIO 0 AND GPIO 2 (GPIO 1 should be not touched)
-	echo 0x27 0x50 > /sys/kernel/debug/iio/iio:device1/direct_reg_access
+	echo 0x27 0x50 > /sys/kernel/debug/iio/iio:device0/direct_reg_access
 	echo 1 > /sys/class/gpio/gpio906/value
 }
 
 pttoff()
 {
-        echo 0x27 0x00 > /sys/kernel/debug/iio/iio:device1/direct_reg_access
+        echo 0x27 0x00 > /sys/kernel/debug/iio/iio:device0/direct_reg_access
 		echo 0 > /sys/class/gpio/gpio906/value
 }
 
 
-echo manual_tx_quad > /sys/bus/iio/devices/iio:device1/calib_mode
+echo manual_tx_quad > /sys/bus/iio/devices/iio:device0/calib_mode
 #Manual GPIO
-echo 0x26 0x10 > /sys/kernel/debug/iio/iio:device1/direct_reg_access
+echo 0x26 0x10 > /sys/kernel/debug/iio/iio:device0/direct_reg_access
 #MIO for plutoplus : MIO start at 906, EMIO at 960
 echo "906" > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio906/direction
@@ -28,8 +28,8 @@ loop()
 {
 while :
 do
-inotifywait -e modify /sys/bus/iio/devices/iio:device1/out_altvoltage1_TX_LO_powerdown
-txmute=$(cat /sys/bus/iio/devices/iio:device1/out_altvoltage1_TX_LO_powerdown)
+inotifywait -e modify /sys/bus/iio/devices/iio:device0/out_altvoltage1_TX_LO_powerdown
+txmute=$(cat /sys/bus/iio/devices/iio:device0/out_altvoltage1_TX_LO_powerdown)
 if [ "$txmute" = "1" ] ; then
 echo "SdrConsole PTT OFF"
 pttoff
